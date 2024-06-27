ef7e581adc88693aa3ef13e36c28df52fbf6a56c
GrayRab <731086651@qq.com>
2024-06-28 03:08:15 +0800

    fixbug调试天气api生效

--- Module/System_8_secret.py
+++ Module/System_8_secret.py
@@ -20,8 +20,20 @@ print(f"随机密码：{Secret3}")
 
 
 import requests
+import json
 city = input("请输入城市名称：")
 api = "27fddef8fc277d3ae2237e5f36ff347b"
-url = f"https://api.openwheathermap.org/data/2.5/weather?q={city}&appid={api}"
-wheather_data = requests.get(url)
-print(wheather_data.json()["main"]["temp"]-273.15) 
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
+'''wheather_data = requests.get(url)
+print(wheather_data.json()["main"]["temp"]) '''
+response = requests.get(url)
+
+if response.status_code == 200:
+    wheather_data = response.json()
+    try:
+        temperature = wheather_data["main"]["temp"] - 273.15
+        print(f"当前温度是：{temperature:.2f}°C")
+    except KeyError:
+        print("无法获取温度数据")
+else:
+    print(f"请求失败，状态码：{response.status_code}")
