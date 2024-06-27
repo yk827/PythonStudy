33241f93678f3b62374abfe77ff3708f2617ba5b
GrayRab <731086651@qq.com>
2024-06-28 02:59:29 +0800

    正常练习，删除天气api接口

--- Module/System_8_secret.py
+++ Module/System_8_secret.py
@@ -17,11 +17,3 @@ print(Secret2)
 length = input("请输入密码长度：")
 Secret3 = "".join(Secret1[:int(length)])
 print(f"随机密码：{Secret3}")
-
-
-import requests
-city = input("请输入城市名称：")
-api = "27fddef8fc277d3ae2237e5f36ff347b"
-url = f"https://api.openwheathermap.org/data/2.5/weather?q={city}&appid={api}"
-wheather_data = requests.get(url)
-print(wheather_data.json()["main"]["temp"]-273.15) 
