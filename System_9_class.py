3d41bf765a7e7f1aac510bbd39adee767763db43
GrayRab <731086651@qq.com>
2024-06-29 20:09:32 +0800

    System_9_class你一拳我一脚的回合制游戏

--- System_9_class.py
+++ System_9_class.py
@@ -13,62 +13,79 @@ class Player_default:
             return 0.5
         elif option == 2:
             print(f"{self.name} 使用闪避！")
-            return random.choice([0.2,1])
+            return random.uniform(0.2,1.0)
 
 class Player_soldier(Player_default):
     def attack(self, option):
         if option == 1:
-            print(f"{self.name} 使用突刺攻击了敌人，造成了{self.attackCount}点伤害！")
+            print(f"{self.name} 使用突刺攻击了敌人")
             return self.attackCount
 
         elif option == 2:
-            print(f"{self.name} 使用斩击攻击了敌人，造成了{self.attackCount*2}点伤害！")
-            return random.choice([self.attackCount, self.attackCount*2])
+            print(f"{self.name} 使用斩击攻击了敌人")
+            return random.uniform(self.attackCount, self.attackCount*2)
+
+        else:
+            print("无效的操作，请重新输入！")
 
 class Player_enermy(Player_default):
     def attack(self, option):
         if option == 1:
-            print(f"{self.name} 使用挥砍攻击了敌人，造成了{self.attackCount}点伤害！")
+            print(f"{self.name} 使用挥砍攻击了敌人")
             return self.attackCount
 
         elif option == 2:
-            print(f"{self.name} 使用偷袭攻击了敌人，造成了{self.attackCount*3}点伤害！")
-            return random.choice([self.attackCount, self.attackCount*3])
+            print(f"{self.name} 使用偷袭攻击了敌人")
+            return random.uniform(self.attackCount, self.attackCount*3)
 
-Player_a = Player_soldier(input("请输入你的名字："), input("请输入你的职业："))
-Player_b = Player_enermy(input("请输入你的名字："), input("请输入你的职业："))
-
-print(f"玩家{Player_a.name}的职业是{Player_a.profession}！")
-print(f"玩家{Player_b.name}的职业是{Player_b.profession}！")
-
-random_enemyChoice = random.choice([1,2])
+        else:
+            print("无效的操作，请重新输入！")    
 
 while True:
-    attack_option = input("请输入你的攻击选项1(突刺)2(斩击)：")
-    if attack_option not in ['1', '2']:
-        print("无效的选项，请重新输入！")
-        continue
-    Player_a_attackAccount = Player_a.attack(attack_option)
-    print(Player_a_attackAccount)  # 调试信息，确保攻击值正确
-    enermy_hp = int(Player_b.defense(random_enemyChoice) * Player_a_attackAccount)
-    Player_b.hp -= enermy_hp
-    if Player_b.hp <= 0:
-        print(f"玩家{Player_b.name}被击败了！")
-        break
-    else:
-        print(f"玩家{Player_b.name}的剩余血量为{Player_b.hp}！")
-        print(f"玩家{Player_a.name}的剩余血量为{Player_a.hp}！")
-        print("")
+    Player_a = Player_soldier(input("请输入你的名字："), input("请输入你的职业："))
+    Player_b = Player_enermy(input("请输入你的名字："), input("请输入你的职业："))
 
-    defense_option = input("请输入你的防御选项1(防御)2(闪避)：")
-    Player_b_attackAccount = Player_b.attack(random_enemyChoice)
+    print(f"玩家{Player_a.name}的职业是{Player_a.profession}！")
+    print(f"玩家{Player_b.name}的职业是{Player_b.profession}！")
 
-    soldier_hp = int(Player_a.defense(defense_option)*Player_b_attackAccount)
-    Player_a.hp -= soldier_hp
-    if Player_a.hp <= 0:
-        print(f"玩家{Player_a.name}被击败了！")
+    random_enemyChoice = random.choice([1,2])
+    # print(type(random_enemyChoice))
+
+    while True:
+        attack_option = input("请输入你的攻击方式1(突刺)2(斩击)：")
+        if attack_option not in ['1', '2']:
+            print("无效的操作，请重新输入！")
+            continue
+        Player_a_attackAccount = Player_a.attack(int(attack_option))
+        # print(Player_a_attackAccount)  # 调试信息，确保攻击值正确
+        enermy_hp = int(Player_b.defense(random_enemyChoice) * Player_a_attackAccount)
+        Player_b.hp -= enermy_hp
+        if Player_b.hp <= 0:
+            print(f"玩家{Player_b.name}被击败了！恭喜{Player_a.name}！")
+            break
+        else:
+            print(f"玩家{Player_a.name}的剩余血量为{Player_a.hp}！")
+            print(f"玩家{Player_b.name}的剩余血量为{Player_b.hp}！")
+            print("")
+
+        defense_option = int(input("请输入你的防御方式1(防御)2(闪避)："))
+        if defense_option not in [1,2]:
+            print("无效的操作，请重新输入！")
+            continue
+        Player_b_attackAccount = Player_b.attack(int(random_enemyChoice))
+        # print(Player_b_attackAccount)  # 调试信息，确保攻击值正确
+        # print(Player_a.defense(defense_option))  # 调试信息，确保防御值正确
+        soldier_hp = int(Player_a.defense(defense_option)*Player_b_attackAccount)
+        Player_a.hp -= soldier_hp
+        if Player_a.hp <= 0:
+            print(f"玩家{Player_a.name}被击败了！恭喜{Player_b.name}！")
+            break
+        else:
+            print(f"玩家{Player_a.name}的剩余血量为{Player_a.hp}！")
+            print(f"玩家{Player_b.name}的剩余血量为{Player_b.hp}！")
+            print("")
+
+    again_choice = input("是否继续游戏？(y/n)：")
+    if again_choice.lower() == 'n':
+        print("游戏结束！")
         break
-    else:
-        print(f"玩家{Player_b.name}的剩余血量为{Player_b.hp}！")
-        print(f"玩家{Player_a.name}的剩余血量为{Player_a.hp}！")
-        print("")
---
+++ test.py
@@ -0,0 +1,26 @@
+import random
+
+t = random.uniform(0.2,1.0)
+print(t)
+
+'''    
+random.uniform(a, b)
+random.random()
+random.randint(a, b)
+random.choice(seq)
+random.shuffle(seq)
+random.sample(seq, k)
+random.gauss(mu, sigma)
+random.expovariate(lambd)
+random.weibullvariate(alpha, beta)
+random.normalvariate(mu, sigma)
+random.lognormvariate(mu, sigma)
+random.vonmisesvariate(mu, kappa)
+random.paretovariate(alpha)
+random.weibull(a, b, c)
+random.triangular(low, high, mode)
+random.gammavariate(alpha, beta)
+random.betavariate(alpha, beta)
+random.pareto(a)
+random.expo(lambd)
+'''
